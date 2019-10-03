719e4bd9597aaff7c3c381213cb5012b984f2bbd
Valerria Hutsuliak <valeria.gutsuliak@gmail.com>
2019-10-03 19:17:11 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,39 +1,56 @@
 def create_an_empty_array
-  
-end
 
-def create_an_array
-  
-end
+  my_array = []
+end	
 
-def add_element_to_end_of_array(array, element)
-  
-end
+
+def create_an_array	
+
+  my_array = ["Lluis","Alvy","Ixax","Esma"]
+end	
+
+
+def add_element_to_end_of_array(array, element)	
+
+  array.push(element)
+end	
+
 
 def add_element_to_start_of_array(array, element)
-  
-end
 
-def remove_element_from_end_of_array(array)
-  
-end
 
-def remove_element_from_start_of_array(array)
-  
-end
+  array.unshift(element)
+end	
+
+
+def remove_element_from_end_of_array(array)	
+  array.pop
+end	
+
+
+def remove_element_from_start_of_array(array)	
+
+  array.shift
+end	
+
 
 def retrieve_element_from_index(array, index_number)
-  
-end
+  array[index_number]
+end	
 
 def retrieve_first_element_from_array(array)
-  
-end
 
-def retrieve_last_element_from_array(array)
-  
-end
+  array[0]
+end	
 
-def update_element_from_index(array, index_number, element)
 
-end
+def retrieve_last_element_from_array(array)	
+
+  array[-1]
+end	
+
+
+def update_element_from_index(array, index_number, element)	
+
+  array[index_number] = element
+end	end
